2486c89d071525998422729f863be3d0c120738f
r33int <me@r33.space>
2019-01-06 13:13:46 +0100

    Add a few comments

--- bot.py
+++ bot.py
@@ -15,10 +15,12 @@ ipinfotoken = conf.get('Settings', 'ipinfo-api-token')
 bot = telebot.TeleBot(tgtoken)
 print("Bot connected")
 
+# /start, /help commands
 @bot.message_handler(commands=['start', 'help'])
 def command_welcome(message):
     bot.send_message(message.chat.id, 'Welcome to the ipinfo bot! To use me, all you have to do is to enter the command /ipinfo <ip address>, and I will give you all the info I got related to this IP address.')
 
+# /source command
 @bot.message_handler(commands=['source'])
 def command_source(message):
     bot.send_message(message.chat.id, 'This software is fully open-source. Source code is available right there: https://github.com/r33int/ipinfo-bot')
@@ -32,6 +34,7 @@ def command_ipinfo(message):
     status = extract_arg(message.text)
     statusstr = ''.join(str(e) for e in status)
 
+    # Check if the IPv4 address is valid
     def is_valid_ipv4_address(address):
         try:
             socket.inet_pton(socket.AF_INET, address)
@@ -51,6 +54,7 @@ def command_ipinfo(message):
         handler = ipinfo.getHandler(ipinfotoken)
         ip_address = (statusstr)
         details = handler.getDetails(ip_address)
+        # very dirty, must be cleaned
         bot.send_message(message.chat.id, '- IP info for ' + statusstr +': -\n' + '\nHostname: ' + details.hostname + '\nCity: ' + details.city + '\nRegion: ' + details.region + '\nContry: ' + details.country + '\nLocation: ' + details.loc + '\nPostcode: ' + details.postal)
     else:
         bot.send_message(message.chat.id, 'This IP address is not valid.')
