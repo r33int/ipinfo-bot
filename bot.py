c4c743d061229f5d789c8dae54a0f977a0a74ef6
r33int <me@r33.space>
2019-01-09 15:31:50 +0100

    Use a function instead of putting everything in the command.

--- bot.py
+++ bot.py
@@ -32,39 +32,10 @@ def extract_arg(arg):
 @bot.message_handler(commands=['ipinfo'])
 def command_ipinfo(message):
     status = extract_arg(message.text)
-    statusstr = ''.join(str(e) for e in status)
+    statusstr = ''.join(status)
+    getinfo(statusstr)
 
-    # Check if the IPv4 address is valid
-    def is_valid_ipv4_address(address):
-        try:
-            socket.inet_pton(socket.AF_INET, address)
-        except AttributeError:
-            try:
-                socket.inet_aton(address)
-            except socket.error:
-                return False
-            return address.count('.') == 3
-        except socket.error:
-            return False
-
-        return True
-
-    if is_valid_ipv4_address(statusstr):
-        print("yes")
-        handler = ipinfo.getHandler(ipinfotoken)
-        ip_address = (statusstr)
-        details = handler.getDetails(ip_address)
-
-        # # store info into variables
-        # det-hostname = details.hostname
-        # det-city = details.city
-        # det-region = details.region
-        # det-country = details.country
-        # det-location = details.location
-        # det-postcode = details.postal
-        # # very dirty, must be cleaned
-        # bot.send_message(message.chat.id, '- IP info for ' + statusstr +': -\n' + '\nHostname: ' + det-hostname + '\nCity: ' + det-city + '\nRegion: ' + det-region + '\nCountry: ' + det-country + '\nLocation: ' + det-location + '\nPostcode: ' + det-postcode)
-    else:
-        bot.send_message(message.chat.id, 'This IP address is not valid.')
+def getinfo(currip):
+    print(currip)
 
 bot.polling(none_stop=1, interval=0, timeout=100000)
